e0dd73e1e9e2248fa6fd4c24d510a4251f9ddaf5
Giorgi.Gujarelidze <gujarelidze@gmail.com>
2025-03-07 16:37:15 +0400

    add environmental variable for browser

--- Unit5practiceAccuweather/Framework/Hooks/Hooks.cs
+++ Unit5practiceAccuweather/Framework/Hooks/Hooks.cs
@@ -7,12 +7,21 @@ namespace Unit5practiceAccuweather.Framework.Hooks
     [Binding]
     public class Hooks
     {
-        private readonly Browser _browser = AqualityServices.Browser;
+        
+        private Browser _browser = null!;
         private static readonly JsonSettingsFile _settings = new JsonSettingsFile("config.json");
+
+        public Hooks()
+        {
+            string browserName = Environment.GetEnvironmentVariable("BROWSER") ?? "chrome";
+            Environment.SetEnvironmentVariable("browserName", browserName);
+
+        }
 
         [BeforeScenario]
         public void Setup()
         {
+            _browser = AqualityServices.Browser;
             _browser.Maximize();
             _browser.GoTo(_settings.GetValue<string>("url"));
         }
